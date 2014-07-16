7b9ed281433e22ef6d6065f648b09c0cec0d7509
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-07-15 17:33:01 -0700

    add basic analytics functionality

--- firefox_client/lib/main.js
+++ firefox_client/lib/main.js
@@ -15,6 +15,7 @@ var { Frame } = require("sdk/ui/frame");
 
 var tab_urls = {};
 var tab_indexes = {};
+var tab_properties = {};
 
 // main
 
@@ -62,17 +63,33 @@ var toolbar = ui.Toolbar({
 });
 
 function next_page() {
-  tab_indexes[activeTab()] += 1;
-  load_url(tab_urls[activeTab()][tab_indexes[activeTab()]], activeTab());
+  //tab_indexes[activeTab()] += 1;
+  //load_url(tab_urls[activeTab()][tab_indexes[activeTab()]], activeTab());
+
+  //new
+  var tab = activeTab();
+  if (tab_properties.hasOwnProperty(tab)) {
+    tab_properties[tab]["url_index"] += 1;
+    load_url(tab_properties[tab]["urls"][tab_properties[tab].url_index], tab)
+  };
+  send_event("next");
 };
 
 function previous_page() {
-  tab_indexes[activeTab()] -= 1;
-  load_url(tab_urls[activeTab()][tab_indexes[activeTab()]], activeTab());
+  //tab_indexes[activeTab()] -= 1;
+  //load_url(tab_urls[activeTab()][tab_indexes[activeTab()]], activeTab());
+
+  //new
+  var tab = activeTab();
+  if (tab_properties.hasOwnProperty(tab)) {
+    tab_properties[tab].url_index -= 1;
+    load_url(tab_properties[tab]["urls"][tab_properties[tab].url_index], tab)
+  };
+  send_event("previous");
 };
 
 //launch button
-var launch = ui.ActionButton({
+/*var launch = ui.ActionButton({
   id: "launch",
   label: "launch",
   icon: "./icons/forward-20.png",
@@ -81,7 +98,33 @@ var launch = ui.ActionButton({
 
 function launch_toolbar() {
   console.log('Launch toolbar clicked');
-};
+};*/
+
+
+//analytics
+
+function request_token() {
+
+}
+
+function send_event(action_event) {
+  tab = activeTab();
+  params = {};
+  params["user_token"] = "U4jTTQOrfNu7gvq_RpaiJQ";
+  params["query"] = tab_properties["query"]
+  params["event"] = action_event;
+  params["nonce"] = Math.floor(Math.random() * Math.pow(2,31));
+
+  //post request to send event
+  var req = Request({
+    url: "http://localhost:3000/api/v1/is_event",
+    content: params,
+    onComplete: function(response) {
+      console.log(response.text);
+    }
+  }).post();
+}
+
 
 //main logic
 
@@ -97,20 +140,29 @@ function getUrls(query, source) {
     url: url_,
     content: query,
     onComplete: function(response) {
-      handleResponse(response, activeTab())
+      handleResponse(response, activeTab(), query)
     }
   }).get();
-
   //console.log("message source id: " + source.ownerID);
 };
 
-function handleResponse(response, source) {
+function handleResponse(response, source, query) {
   var urls = JSON.parse(response.text).urls;
-  console.log("urls ----------------- : " + urls.length);
-  tab_urls[source] = urls;
-  tab_indexes[source] = 0;
+  //console.log("urls ----------------- : " + urls.length);
+
+  //tab_urls[source] = urls;
+  //tab_indexes[source] = 0;
+  //load_url(tab_urls[source][0], source);
+  //new
+  tab_properties[source] = {};
+  tab_properties["query"] = query;
+  tab_properties[source]["urls"] = urls;
+  tab_properties[source]["url_index"] = 0;
+  //console.log(tab_properties[source]["urls"])
   //console.log("id of active tab is " + source);
-  load_url(tab_urls[source][0], source);
+  load_url(tab_properties[source]["urls"][0], source);
+
+  send_event("search");
 };
 
 function load_url(url, tabid) {
