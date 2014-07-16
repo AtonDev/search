08fb42bf41f8c988b83b77b388299268560f2d5e
Jason <jasonclark@MacBook-Pro-di-Nicholas.local>
2014-07-16 13:09:30 -0700

    basic navbar implemented

--- chrome_client/background.js
+++ chrome_client/background.js
@@ -1,14 +1,94 @@
-var urls, tabId, curIndex = 0;
+var tabStates = {};
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (tabId == sender.tab.id) {
-      if (request.action == "right") {
-        curIndex+=1;
-      }else if (request.action == "left") {
-        curIndex -= 1;
-      }
-      chrome.tabs.update(sender.tab.id, {url: urls[curIndex]}); 
-    }
+    id = sender.tab.id;
+    switch (request.action) {
+        case 'search':
+            submitToServer(id, request.query);           
+            break;
+
+        case 'next':
+            nextPage(id); 
+            break;
+
+        case 'loaded': //TODO add timeout if newtab's onCreated hasn't fired yet so tabState can be created first
+            console.log('received loaded message');
+            tabState = tabStates[id];
+            sendResponse({action: "populateSearchBox", query: tabStates[id].query});
+            break;
+
+        case 'removeToolbar':
+            console.log(id);
+            chrome.tabs.sendMessage(id, {action: "removeToolbar"});
+            tabStates[id].state = 'off';
+            tabStates[id].query = '';
+            break;
+
+        case 'getTabState':
+            sendResponse({state: tabStates[id].state, query: tabStates[id].query});
+            break;
+
+        case 'right':
+            nextPage(id);
+            break;
+        case 'left':
+            nextPage(id, "back");
+            break;
+     }
   }
 );
+
+function nextPage(id, dir) {
+  var tabState = tabStates[id];
+  var idx = tabState.idx;
+  var newIndex;
+  var urls = tabState.urls;
+  if (dir == "back") {
+     newIndex = (idx > 0) ? idx-1 : 0
+  }else newIndex = idx + 1;
+  tabStates[id].idx = newIndex
+  chrome.tabs.update(id, {url: urls[newIndex]}); 
+}
+
+
+function submitToServer(tabId, query) {
+  splitQuery = query.split(' ').join('+');
+  var url = "http://instantsearch.herokuapp.com/s?search=" + splitQuery;
+  console.log(url);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var urls = JSON.parse(xhr.responseText).urls;
+        tabStates[tabId].urls = urls; 
+        tabStates[tabId].idx = 0;
+        tabStates[tabId].query = query;
+        chrome.tabs.update(tabId, {url: urls[0]}); 
+      }else console.log("no 200 status");
+    }else console.log("readyState not 4 instead: " + xhr.readyState);      
+  }
+  xhr.send();
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  var id = tab.id;
+  console.log(id);
+  tabStates[id] = {};
+  tabStates[id].state = 'on';
+  tabStates[id].query = '';
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var id = tab.id
+  var toolbarExists = id in tabStates;
+  console.log("browser action detected");
+  if(!toolbarExists || tabStates[id].state == 'off') {
+    tabStates[id] = {};
+    tabStates[id].state = 'on';
+    tabStates[id].query = '';
+    chrome.tabs.sendMessage(id, {action: "loadToolbar"});
+  } 
+});
--- chrome_client/content_script.js
+++ chrome_client/content_script.js
@@ -1,3 +1,27 @@
+var toolbarHeight = '40';
+var newCssText = "margin-top: " + toolbarHeight + "px !important";
+document.body.style.position = "relative"; 
+var newIframe = document.createElement('iframe');
+newIframe.width = '100%';
+newIframe.height = toolbarHeight + "px";
+newIframe.id = "searchToolbar"
+newIframe.style.border = 'none';
+newIframe.style.position = "fixed";
+newIframe.style.top = "0px";
+newIframe.style.left = "0px";
+newIframe.style.margin = "0px";
+newIframe.style.zIndex = "999999";
+//newIframe.src = chrome.extension.getURL("toolbar.html");
+window.onload = init();
+
+
+
+function getTabState() {
+	chrome.runtime.sendMessage({action: "getTabState"}, function(response) {
+		console.log(response.state)
+	});
+}
+
 document.onkeydown = checkKeyPress;
 function checkKeyPress(e) {
   e = e || window.event;
@@ -9,4 +33,63 @@ function checkKeyPress(e) {
   //left arrow pressed
     chrome.runtime.sendMessage({action: "left"});
   }
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    	console.log("received message: " + request.action)
+        if (request.action == "removeToolbar")
+ 			removeToolbar(); 
+ 		else if(request.action == "loadToolbar")
+ 		    init();    
+});
+
+function moveElementsByTag(tag, change) {
+	var elements = document.getElementsByTagName(tag); //might need to add "header" tag
+	for (var elNum = 0; elNum < elements.length; elNum++){
+		var el = elements[elNum];
+		var elStyle = window.getComputedStyle(el);
+		if (elStyle.position == "fixed"){
+			var top = elStyle.top;
+			var newtop = top ? parseInt(top) : 0;
+			el.style.top = newtop + change + "px";
+		}
+	}
+}
+
+function moveFixedElements(change) {
+	moveElementsByTag("div", change);
+	moveElementsByTag("header", change)
+}
+
+function removeToolbar() {
+	var toolbar = document.getElementById(newIframe.id);
+	if (toolbar) {
+	    toolbar.parentNode.removeChild(toolbar);
+	    moveFixedElements(toolbarHeight * -1);
+	}
+	cssText = document.body.style.cssText;
+	cssText = cssText.replace(newCssText, "margin-top: 0px");
+	document.body.style.cssText = cssText;
+}
+
+function getTabState() {
+	chrome.runtime.sendMessage({action: "getTabState"}, function(response) {
+		if (response.state == 'on') {
+			if (response.query == '') {
+				//no query has been made, so load toolbar without next button
+				newIframe.src = chrome.extension.getURL("toolbarInitial.html");
+			}else {
+				//load toolbar with next button
+				newIframe.src = chrome.extension.getURL("toolbar.html");
+			}
+			document.body.style.cssText = document.body.style.cssText + ";" + newCssText;
+			moveFixedElements(toolbarHeight);
+			document.body.insertBefore(newIframe, document.body.firstChild);
+	    } 
+	});
+}
+
+function init() {
+	getTabState();
 }
