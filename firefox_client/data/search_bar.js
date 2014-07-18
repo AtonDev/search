19c5fdb339cd8c64f0a91bfda4ff34651f7eafc3
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-07-18 12:40:36 -0700

    complete basic ui; solve some bugs;

--- firefox_client/data/search_bar.js
+++ firefox_client/data/search_bar.js
@@ -1,14 +1,22 @@
+var current_query = "";
+
+
 var btn = window.document.getElementById("submit-query-btn");
 btn.addEventListener("click", submit);
 
 var query_box = window.document.getElementById("query-box");
-query_box.addEventListener("focus", typing);
+query_box.addEventListener("focus", searchEnvironment);
+query_box.addEventListener("blur", loses_focus);
+query_box.addEventListener("change", searchEnvironment);
 
 window.addEventListener("message", handleResponse, false);
+
+
 
 function submit() {
   switch(btn.value) {
     case "Search":
+      current_query = query_box.value;
       window.parent.postMessage({
         "type" : "search",
         "query" : query_box.value
@@ -23,25 +31,39 @@ function submit() {
   };
 }
 
-function typing() {
-  query_box.value = "";
-  query_box.placeholder = "Explore the web";
-  btn.value = "Search";
-  btn.style.background = "#617798";
 
+function loses_focus() {
+  if (current_query != "") {
+    nextEnvironment(current_query);
+  };
 }
 
 function handleResponse (message) {
   switch(message.data.type) {
     case "btn-change-next":
-      btn.value = "Next";
-      btn.style.background = "#009933";
-      query_box.placeholder = "query: " + message.data.query;
+      nextEnvironment(message.data.query);
       break;
     case "btn-change-search":
-      btn.value = "search";
-      query_box.placeholder = "Explore the web";
-      btn.style.background = "#617798";
+      searchEnvironment();
+      current_query = "";
       break;
   };
 }
+
+function searchEnvironment() {
+  btn.value = "Search";
+  query_box.placeholder = "Explore the web";
+  btn.style.background = "#617798";
+}
+
+function nextEnvironment(query) {
+  btn.value = "Next";
+  btn.style.background = "#009933";
+  query_box.placeholder = "query: " + query;
+  current_query = query;
+}
+
+
+
+
+
