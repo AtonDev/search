fd38ad7b515ca290a97a7fd5b1e690b147befd57
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-07-24 17:09:51 -0700

    add all possible prefetch links

--- firefox_client/data/prefetch.js
+++ firefox_client/data/prefetch.js
@@ -1,7 +1,17 @@
 self.port.on("link", function(url) {
-  var nextPage = document.createElement("link");
-  nextPage.rel = "next";
-  nextPage.href = url;
-  document.getElementsByTagName("head")[0].appendChild(nextPage);
+  var nextPage1 = document.createElement("link");
+  nextPage1.rel = "prefetch";
+  nextPage1.href = url;
+  document.getElementsByTagName("head")[0].appendChild(nextPage1);
+
+  var nextPage2 = document.createElement("link");
+  nextPage2.rel = "next";
+  nextPage2.href = url;
+  document.getElementsByTagName("head")[0].appendChild(nextPage2);
+
+  var meta = document.createElement('meta');
+  meta.httpEquiv = "Link";
+  meta.content = "<" + url + ">; rel=prefetch";
+  document.getElementsByTagName("head")[0].appendChild(meta);
 });
 
